5ca1e943ca20d3f46802a8cfd91f1587e50db2af
S17S17 <164513807+S17S17@users.noreply.github.com>
2025-03-24 01:54:14 +0100

    Add nodeConfiguration module

---
+++ src/nodeConfiguration/index.ts
@@ -0,0 +1,8 @@
+/**
+ * Node Configuration Module for AI Nodes
+ */
+
+export * from './nodeTypes';
+export * from './modelValidation';
+export * from './errorHandling';
+export * from './nodePropertyTemplates';
